95a789535f5b5642955318542a44b7e42a6e39f9
咔咔 <874882639@qq.com>
2020-05-09 19:30:28 +0800

    嘿嘿

---
+++ src/views/home/home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="home">
+        主页
+    </div>
+</template>
+<script>
+    import { getHomeMultidata } from "../service/home"
+    // import HomeSwiper from './childComps/HomeSwiper'
+    
+    export default {
+        name: 'Home',
+        components: {
+            // HomeSwiper,
+        },
+        data() {
+            return {
+                banners: null,
+                listDatas: null,
+            }
+        },
+        
+        created() {
+            getHomeMultidata().then(res=> {
+                console.log('res',res)
+            }).catch(err=> {
+                console.log(err)
+            }) 
+        }
+    }
+</script>
+<style>
+    
+</style>
